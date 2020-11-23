86014ec22ad835fc17cf09384abda058b11d3b76
Dima Rashevskyi <31186934+drashevskyi@users.noreply.github.com>
2020-11-23 08:42:31 +0300

    rm

--- routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PhotoController;
-
-Route::get('/', [PhotoController::class, 'index'])->name('index');
-Route::post('photos/store', [PhotoController::class, 'store'])->name('store');
